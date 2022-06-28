805090e98801dd4e7371888330b59b07a61dc1ab
Narmadha Raghu <narmadha.r@ionixxtech.com>
2022-06-28 14:18:10 +0000

    host configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts = "ec2-13-233-44-4.ap-south-1.compute.amazonaws.com"
 end
